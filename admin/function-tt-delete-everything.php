da0ecf4a626f2f69f1b19a70b3b24926225e5697
GermanPearls <amy@logicallytech.com>
2020-11-13 13:20:36 -0500

    don't call core WP files directly
    updated required class to a relative path
    (feedback from WP plugin review)

--- admin/function-tt-delete-everything.php
+++ admin/function-tt-delete-everything.php
@@ -10,21 +10,11 @@
  */
 
 
-/**
- * If wordpress isn't loaded load it up
- * 
- */
-if ( !defined('ABSPATH') ) {
-    $path = $_SERVER['DOCUMENT_ROOT'];
-    include_once $path . '/wp-load.php';
-}
-
-
 if ( ($_SERVER['REQUEST_METHOD'] = 'POST') && isset($_POST["type"]) ){
 
     if ($_POST["type"] == "confirmed") {
     	
-		require_once $path . '/wp-content/plugins/time-tracker/inc/class-time-tracker-delete.php';
+		require_once '/../inc/class-time-tracker-delete.php';
         Time_Tracker_Deletor::delete_all();
         
     }
