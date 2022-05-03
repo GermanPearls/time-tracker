892153c578a33492a40e553afab418b302a0d4ff
GermanPearls <amy@logicallytech.com>
2022-05-03 11:17:56 -0400

    fix

--- inc/js/set_date_picker_default_value.js
+++ inc/js/set_date_picker_default_value.js
@@ -1,15 +1,15 @@
 //Set default values for date pickers, if they exist in get value
 document.addEventListener('DOMContentLoaded', function () {  //make sure doc is done loading before looking for element  
-  var startDateField = document.getElementsByName('start-date');
-  var endDateField = document.getElementsByName('end-date');  
+  var startDateField = document.getElementsByName('firstt-date');
+  var endDateField = document.getElementsByName('last-date');  
 
   if (startDateField || endDateField ) {
     //https://stackoverflow.com/a/901144/7303640
     const params = new Proxy(new URLSearchParams(window.location.search), {
       get: (searchParams, prop) => searchParams.get(prop),
     });
-    let start = params['start-date'];
-    let end = params['end-date'];
+    let start = params['first-date'];
+    let end = params['last-date'];
 
     if (startDateField && start) {
       startDateField.value = start;
