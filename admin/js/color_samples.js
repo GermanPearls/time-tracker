a49a2a254c4ec24bdc61e57596cf761614884fa3
GermanPearls <amy@logicallytech.com>
2022-08-17 09:39:37 -0400

    resolve console warnings

--- admin/js/color_samples.js
+++ admin/js/color_samples.js
@@ -1,21 +1,31 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var bk = document.getElementById('tt-css-buttons-background-normal');
-    bk.addEventListener('input', function() {
-        document.getElementById('tt-color-display-buttons-background-normal').style.backgroundColor = this.value;    
-    });
+	if (document.getElementById('tt-css-button-override')) {
+		var bk = document.getElementById('tt-css-buttons-background-normal');
+		if (bk) {
+			bk.addEventListener('input', function() {
+				document.getElementById('tt-color-display-buttons-background-normal').style.backgroundColor = this.value;    
+			});
+		}
 
-    var txt = document.getElementById('tt-css-buttons-text-normal');
-    txt.addEventListener('input', function() {
-        document.getElementById('tt-color-display-buttons-text-normal').style.backgroundColor = this.value;    
-    });
+		var txt = document.getElementById('tt-css-buttons-text-normal');
+		if (txt) {
+			txt.addEventListener('input', function() {
+				document.getElementById('tt-color-display-buttons-text-normal').style.backgroundColor = this.value;    
+			});
+		}
 
-    var bkhover = document.getElementById('tt-css-buttons-background-hover');
-    bkhover.addEventListener('input', function() {
-        document.getElementById('tt-color-display-buttons-background-hover').style.backgroundColor = this.value;    
-    });
+		var bkhover = document.getElementById('tt-css-buttons-background-hover');
+		if (bkhover) {
+			bkhover.addEventListener('input', function() {
+				document.getElementById('tt-color-display-buttons-background-hover').style.backgroundColor = this.value;    
+			});
+		}
 
-    var txthover = document.getElementById('tt-css-buttons-text-hover');
-    txthover.addEventListener('input', function() {
-        document.getElementById('tt-color-display-buttons-text-hover').style.backgroundColor = this.value;    
-    });
+		var txthover = document.getElementById('tt-css-buttons-text-hover');
+		if (txthover) {
+			txthover.addEventListener('input', function() {
+				document.getElementById('tt-color-display-buttons-text-hover').style.backgroundColor = this.value;    
+			});
+		}
+	}
 });
