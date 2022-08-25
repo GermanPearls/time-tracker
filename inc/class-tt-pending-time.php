821bdcc6a0b86f023488fb27147840895f64e0f3
GermanPearls <amy@logicallytech.com>
2022-08-25 11:13:08 -0400

    finish converting pending time to new table class

--- inc/class-tt-pending-time.php
+++ inc/class-tt-pending-time.php
@@ -185,7 +185,7 @@ if ( !class_exists( 'Pending_Time' ) ) {
                     "editable" => false,
                     "columnwidth" => "",
                     "type" => "long text",
-                    "class" => "tt-align-right"
+                    "class" => ""
                 ],
                 "Invoiced?" => [
                     "fieldname" => "Invoiced",
@@ -248,16 +248,17 @@ if ( !class_exists( 'Pending_Time' ) ) {
             if (empty($time_entries)) {
                 return "<strong>All caught up!</strong>";
             }
-
-            foreach ($time_entries as $item) {
-                $time_estimate_formatted = get_time_estimate_formatted(sanitize_text_field($item->TTimeEstimate));
-                $hours_logged = tt_convert_to_decimal_time(sanitize_text_field($item->LoggedHours), sanitize_text_field($item->LoggedMinutes));
+			//need the ampersand to pass by reference so item gets updated since we converted time_entries from object to array
+            foreach ($time_entries as &$item) {
+                $time_estimate_formatted = get_time_estimate_formatted(sanitize_text_field($item["TTimeEstimate"]));
+                $hours_logged = tt_convert_to_decimal_time(sanitize_text_field($item["LoggedHours"]), sanitize_text_field($item["LoggedMinutes"]));
                 $percent_time_logged = get_percent_time_logged($time_estimate_formatted, $hours_logged);
                 $time_worked_vs_estimate_class = get_time_estimate_class($percent_time_logged);
-                $item->TimeLoggedVsEstimate = [
-                    "value" => $hours_logged . $percent_time_logged,
-                    "class" => $time_worked_vs_estimate_class
-                ];
+				echo("log hours is " . $item["LoggedHours"] . " log min is " . $item["LoggedMinutes"] . " percent time logged is " . $percent_time_logged);
+				$item["TimeLoggedVsEstimate"] = [
+					"value" => $hours_logged . $percent_time_logged,
+					"class" => $time_worked_vs_estimate_class
+				];
             }
             return $time_entries;
         }
@@ -274,92 +275,6 @@ if ( !class_exists( 'Pending_Time' ) ) {
             $table = $tbl->create_html_table($fields, $time_entries, $args, "tt_time", "TimeID");
             return $table;
         }
-        
-        
-        /**
-         * Create individual tables for each bill to
-         * 
-         */ 
-        /**private function create_table($time_entries) {    
-            if (empty($time_entries)) {
-                $table = "<strong>All caught up!</strong>";
-                return $table;
-            }
-
-            //Begin creating table and headers
-            $table = "<strong>Note: Gray shaded cells can't be changed.</strong><br/><br/>";
-            $table .= "<table class=\"tt-table pending-time-table\">";
-            $table .= "<thead><tr>";
-            $table .= "<th>Client</th>";
-            $table .= "<th>Task</th>";
-            $table .= "<th>Task Description</th>";
-            $table .= "<th>Start</th>";
-            $table .= "<th>End</th>";
-            $table .= "<th>Time Logged vs Estimate</th>";
-            $table .= "<th>Invoiced</th>";
-            $table .= "<th>Invoice #</th>";
-            $table .= "<th>Invoiced Time</th>";
-            $table .= "<th>Invoice Notes</th>";
-            $table .= "<th>Status</th>";            
-            $table .= "<th>Notes</th>";
-            $table .= "</tr></thead>";
-
-            $previous_client = sanitize_text_field($time_entries[0]['Company']);
-
-            //Create body
-            foreach ($time_entries as $item) {
-                $client = sanitize_text_field($item['Company']);
-                $logged_hours = sanitize_text_field($item['LoggedHours']);
-                $logged_minutes = sanitize_text_field($item['LoggedMinutes']);
-                $time_estimate = sanitize_text_field($item['TTimeEstimate']);
-                $taskid = sanitize_text_field($item['TaskID']);
-                $timeid = sanitize_text_field($item['TimeID']);
-                $starttime = sanitize_text_field($item['StartTime']);
-                $endtime = sanitize_text_field($item['EndTime']);
-
-                if ($previous_client !== $client) {
-                    $table .= "<tr><td class=\"divider-row\" colspan=\"12\"></td></tr>";
-                }
-
-                $ticket = $taskid . "-" . $item['TDescription'];
-        
-                $time_fraction_logged = (float)$logged_hours + round((float)$logged_minutes/60,2);
-                $time_logged = tt_convert_to_string_time((float)$logged_hours, (float)$logged_minutes);
-                if (($time_estimate != null) && ($time_estimate != 0)) {
-                    $time_estimate_parts = explode(":", $time_estimate);
-                    $time_estimate_as_number = tt_convert_to_decimal_time($time_estimate_parts[0], $time_estimate_parts[1]);
-                    $percent_time_logged = " / " . $time_estimate_as_number . "<br/>" . round($time_fraction_logged / $time_estimate_as_number * 100) . "%";
-                } else {
-                    $percent_time_logged = "";
-                }
-
-                //create row
-                $table .= "<tr>";           
-                $table .= "<td id=\"client\" class=\"not-editable tt-col-width-ten-pct\">" . esc_textarea($client) . "</td>";
-                $table .= "<td id=\"task-id\" class=\"tt-editable tt-col-width-five-pct\" contenteditable=\"true\" onBlur=\"updateDatabase(this, 'tt_time', 'TimeID', 'TaskID'," . esc_attr($timeid) . ")\">" . esc_textarea($taskid) . "</td>";
-                $table .= "<td id=\"task-description\" class=\"not-editable tt-col-width-fifteen-pct\">"  . wp_kses_post(nl2br($item['TDescription'])) . "</td>";
-                $table .= "<td id=\"start-time\" class=\"not-editable tt-col-width-five-pct\">" . esc_textarea(tt_format_date_for_display($starttime, "date_and_time")) . "</td>";
-                $table .= "<td id=\"end-time\" class=\"not-editable tt-col-width-five-pct\">" . esc_textarea(tt_format_date_for_display($endtime, "date_and_time")) . "</td>";
-                $table .= "<td id=\"time-logged\" class=\"not-editable tt-col-width-five-pct\">" . $time_logged . "<br/>" . $time_fraction_logged . " hrs" . $percent_time_logged . "</td>";
-                $table .= "<td id=\"invoiced\" class=\"tt-editable tt-col-width-five-pct\" contenteditable=\"true\" onBlur=\"updateDatabase(this, 'tt_time', 'TimeID', 'Invoiced'," . esc_attr($timeid) . ")\">" . esc_textarea(sanitize_text_field($item['Invoiced'])) . "</td>";
-                $table .= "<td id=\"invoice-number\" class=\"tt-editable tt-col-width-five-pct\" contenteditable=\"true\" onBlur=\"updateDatabase(this, 'tt_time', 'TimeID', 'InvoiceNumber'," . esc_attr($timeid) . ")\">" . esc_textarea(sanitize_text_field($item['InvoiceNumber'])) . "</td>";
-                $table .= "<td id=\"invoiced-time\" class=\"tt-editable tt-col-width-five-pct\" contenteditable=\"true\" onBlur=\"updateDatabase(this, 'tt_time', 'TimeID', 'InvoicedTime'," . esc_attr($timeid) . ")\">" . esc_textarea(sanitize_text_field($item['InvoicedTime'])) . "</td>";
-                $table .= "<td id=\"invoice-notes\" class=\"tt-editable tt-col-width-five-pct\" contenteditable=\"true\" onBlur=\"updateDatabase(this, 'tt_time', 'TimeID', 'InvoiceComments'," . esc_attr($timeid) . ")\">" . wp_kses_post(nl2br($item['InvoiceComments'])) . "</td>";
-                $table .= "<td id=\"status\" class=\"tt-editable tt-col-width-five-pct\" contenteditable=\"true\" onBlur=\"updateDatabase(this, 'tt_task', 'TaskID', 'TStatus'," . esc_attr($taskid) . "), updateDatabase(this, 'tt_time', 'TimeID', 'NewTaskStatus'," . esc_attr($timeid) . ")\">" . esc_textarea(sanitize_text_field($item['NewTaskStatus'])) . "</td>";
-                $table .= "<td id=\"task-notes\" class=\"tt-editable tt-col-width-thirty-pct\" contenteditable=\"true\" onBlur=\"updateDatabase(this, 'tt_time', 'TimeID', 'TNotes'," . esc_attr($timeid) . ")\">" . wp_kses_post(nl2br($item['TNotes'])) . "</td>";
-                //close out row
-                $table .="</tr>";
-
-                $previous_client = $client;
-
-            } // foreach loop
-
-            //close out table
-            $table .= "</table>";
-
-            return $table;
-        } //close function to create to do list table for display
-        **/
 
     } //close class
 
