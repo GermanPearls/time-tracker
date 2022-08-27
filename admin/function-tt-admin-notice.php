1b494004c3ac9f9a472593addf157d04a807de6f
GermanPearls <amy@logicallytech.com>
2022-08-27 00:21:38 -0400

    add dismiss button

--- admin/function-tt-admin-notice.php
+++ admin/function-tt-admin-notice.php
@@ -8,25 +8,32 @@
  * 
  */
 
-namespace Logically_Tech\Time_Tracker\Admin;
-
 function tt_feedback_request() {
-    $msg = "<div class=\"notice notice-info is-dismissable tt-admin-notice\">";
-    $msg .= "<p>Thank you for trying the Time Tracker plugin. We\'d love to hear your feedback.";
+	$msg = "<div class='notice notice-info is-dismissable tt-admin-notice'>";
+    $msg .= "<p><h3>Thank you for trying the Time Tracker plugin.</h3>";
+	$msg .= "We'd love to hear your feedback. ";
     $msg .= "Feel free to reach out directly with issues or recommendations at ";
-    $msg .= "<a href=\"mailto:info@logicallytech.com\">info@logicallytech.com</a>.";
-    $msg .= "If you\'re enjoying the plugin and could take a few moments to leave a review, ";
-    $msg .= "it would be greatly appreciated.";
-    $mgs .= "<button href=\"https:\/\/wordpress.org\/support\/plugin\/time-tracker\/reviews\/#new-post\">";
-    $msg .= "Leave a Review<\/button>";
-    $msg .= "<\/p><\/div>";
+    $msg .= "<a href='mailto:info@logicallytech.com'>info@logicallytech.com</a>. ";
+    $msg .= "If you're enjoying the plugin and could take a few moments to leave a review, ";
+    $msg .= "it would be greatly appreciated.<br/>";
+    $msg .= "<button href='https://wordpress.org/support/plugin/time-tracker/reviews/#new-post' ";
+	$msg .= "style='padding: 5px 15px; margin-top:15px;'>";
+    $msg .= "Leave a Review</button>";
+	$msg .= tt_dismiss_notice_button("feedback_request", 6);
+    $msg .= "</p></div>";
     return $msg;    
 }
 
-
-function tt_admin_notice() {
-    $notice = tt_feedback_request();
-    echo $notice;
+function tt_dismiss_notice_button($notice, $mnths) {
+	$btn = "<button onclick='tt_dismiss_admin_notice(" . $notice . ", " . $mnths . ")' ";
+	$btn .= "style='padding: 5px 15px; margin-left:15px; margin-top:15px;'>";
+	$btn .= "Dismiss for " . $mnths . " Months</button>";
+	return $btn;
 }
 
-add_action( 'admin_notices', 'tt_admin_notice' );
+
+function tt_dashboard_notice() {
+    echo tt_feedback_request();
+}
+
+add_action( 'admin_notices', 'tt_dashboard_notice' );
