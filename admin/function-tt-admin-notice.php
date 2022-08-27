515c8e20e65e5251740b290c7acee77f4d66bb98
GermanPearls <amy@logicallytech.com>
2022-08-26 22:31:06 -0400

    escape apostrophes

--- admin/function-tt-admin-notice.php
+++ admin/function-tt-admin-notice.php
@@ -14,10 +14,10 @@ function tt_display_admin_notice() {
     global $pagenow;
     if ( $pagenow == 'index.php' ) {
           echo '<div class="notice notice-info is-dismissible tt-admin-notice">
-          <p>Thank you for trying the Time Tracker plugin. We'd love to hear your feedback.
+          <p>Thank you for trying the Time Tracker plugin. We\'d love to hear your feedback.
           Feel free to reach out directly with any issues or recommendations at 
           <a href="mailto:info@logicallytech.com">info@logicallytech.com</a>.
-          If you're enjoying the plugin and could take a few moments to leave a review 
+          If you\'re enjoying the plugin and could take a few moments to leave a review 
           it would be greatly appreciated.
           <button onclick=window.location.href="https://wordpress.org/support/plugin/time-tracker/reviews/#new-post">
           Leave a Review</button></p>
