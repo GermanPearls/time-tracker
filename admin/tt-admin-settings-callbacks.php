47471870a1efc92c174ed4d0bcd2b84948046adf
GermanPearls <amy@logicallytech.com>
2022-08-27 23:14:03 -0400

    update option names

--- admin/tt-admin-settings-callbacks.php
+++ admin/tt-admin-settings-callbacks.php
@@ -34,7 +34,7 @@ function tt_style_section_callback() {
  */
 function tt_categories_bill_to_names_callback() {
     //get the value if it's already been entered
-    $setting = get_option('time-tracker');
+    $setting = get_option('time_tracker_categories');
 
     //display on menu page
     ?>
@@ -43,8 +43,8 @@ function tt_categories_bill_to_names_callback() {
     Do you perform services under different business names?<br>
     In the section below, add your different bill to names, one per line.<br>
     Examples: Self, Client, Third Party Business Name<br><br>
-    <textarea id="tt-bill-to" name="time-tracker[bill-to-names]" rows="5" cols="30" class="tt-options-form" form="tt-options"><?php
-    $btn = trim(sanitize_textarea_field($setting['bill-to-names']));
+    <textarea id="tt-bill-to" name="time_tracker_categories[bill_to_names]" rows="5" cols="30" class="tt-options-form" form="tt-options"><?php
+    $btn = trim(sanitize_textarea_field($setting['bill_to_names']));
     if (isset( $btn )) {
         echo esc_html($btn);
     } else {
@@ -62,7 +62,7 @@ function tt_categories_bill_to_names_callback() {
  */
 function tt_categories_work_categories_callback() {
     //get the value if it's already been entered
-    $setting = get_option('time-tracker');
+    $setting = get_option('time_tracker_categories');
 
     //display on menu page
     ?>
@@ -70,8 +70,8 @@ function tt_categories_work_categories_callback() {
     In the section below, add the options you'd like to see for this field when entering a new project or task.<br>
     Examples: Website Design, Social Media Management, Website Updates.<br>
     Enter one category on each line.<br><br>
-    <textarea id="tt-categories" name="time-tracker[work-categories]" rows="10" cols="30" class="tt-options-form" form="tt-options"><?php
-    $wc = trim(sanitize_textarea_field($setting['work-categories']));
+    <textarea id="tt-categories" name="time_tracker_categories[work_categories]" rows="10" cols="30" class="tt-options-form" form="tt-options"><?php
+    $wc = trim(sanitize_textarea_field($setting['work_categories']));
     if (isset( $wc )) {
         echo esc_html($wc);
     } else {
@@ -89,15 +89,15 @@ function tt_categories_work_categories_callback() {
  */
 function tt_categories_client_categories_callback() {
     //get the value if it's already been entered
-    $setting = get_option('time-tracker');
+    $setting = get_option('time_tracker_categories');
 
     //display on menu page
     ?>
     <div class="tt-indent">Time Tracker will maintain information on each of your clients. When adding a new client you'll be asked to enter how they found you.<br>
     In the section below, add the options you'd like to see for this field, one per line. <br>
     Examples: Paid Ad, Organic Search, Referral.<br><br>
-    <textarea id="tt-csource" name="time-tracker[client-categories]" rows="10" cols="30" class="tt-options-form" form="tt-options"><?php
-    $cc = trim(sanitize_textarea_field($setting['client-categories']));
+    <textarea id="tt-csource" name="time_tracker_categories[client_categories]" rows="10" cols="30" class="tt-options-form" form="tt-options"><?php
+    $cc = trim(sanitize_textarea_field($setting['client_categories']));
     if (isset( $cc )) {
         echo esc_html($cc);
     } else {
@@ -115,15 +115,15 @@ function tt_categories_client_categories_callback() {
  */
 function tt_categories_client_sub_categories_callback() {
     //get the value if it's already been entered
-    $setting = get_option('time-tracker');
+    $setting = get_option('time_tracker_categories');
 
     //display on menu page
     ?>
     <div class="tt-indent">You can also add a second level of information on how the client found you.<br>
     In the section below, add the options you'd like to see for this field, one per line.<br>
     Examples: Google PPC, Facebook Ad, LinkedIn Ad, Name of Individual That Referred Client<br><br>
-    <textarea id="tt-client-sub-categories" name="time-tracker[client-sub-categories]" rows="10" cols="30" class="tt-options-form" form="tt-options"><?php
-    $csc = sanitize_textarea_field($setting['client-sub-categories']);
+    <textarea id="tt-client-sub-categories" name="time_tracker_categories[client_sub_categories]" rows="10" cols="30" class="tt-options-form" form="tt-options"><?php
+    $csc = sanitize_textarea_field($setting['client_sub_categories']);
     if (isset( $csc )) {
         echo esc_html($csc);
     } else {
