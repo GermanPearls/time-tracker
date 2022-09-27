4436ab37236a0501c28a2cd905ab6ee77347d340
GermanPearls <amy@logicallytech.com>
2022-09-27 13:30:53 -0400

    fix - recaptcha now disabled on tt pages only

--- inc/function-tt-recaptcha.php
+++ inc/function-tt-recaptcha.php
@@ -14,8 +14,8 @@ add_action( 'wp_enqueue_scripts', 'time_tracker_remove_cf7_recaptcha', 15);
 function time_tracker_remove_cf7_recaptcha() {
     if(is_singular()) {
         global $post;
-        $post_type = get_post_type( $post->template );
-        if ($post_type = "Time Tracker Page") {
+        $post_type = get_post_meta( get_post()->ID, '_wp_page_template', true );
+        if ($post_type == "tt-page-template.php") {
             remove_action( 'wp_enqueue_scripts', 'wpcf7_recaptcha_enqueue_scripts', 20, 0 );
         }
     }
