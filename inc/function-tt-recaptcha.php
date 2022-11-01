ca3c6a352471fb8ee5c026c6a7c5a360512019fe
GermanPearls <amy@logicallytech.com>
2022-11-01 12:46:26 -0400

    stop recaptcha

--- inc/function-tt-recaptcha.php
+++ inc/function-tt-recaptcha.php
@@ -13,8 +13,9 @@ function time_tracker_remove_cf7_recaptcha() {
     if(is_singular()) {
         $post_type = get_post_meta( get_post()->ID, '_wp_page_template', true );
         if ($post_type == "tt-page-template.php") {
+            add_filter( 'wpcf7_skip_spam_check', '__return_true');
             remove_action( 'wp_enqueue_scripts', 'wpcf7_recaptcha_enqueue_scripts', 20, 0 );
-            add_filter('wpcf7_skip_spam_check', '__return_true');
+            remove_action( 'wpcf7_init', 'wpcf7_recaptcha_add_form_tag_recaptcha', 10, 0);
         }
     }
 }
