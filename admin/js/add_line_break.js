0e82692245bd182a20c8f2d2d42aecd2e8c31c8a
GermanPearls <amy@logicallytech.com>
2023-02-08 14:16:13 -0500

    clean up code

--- admin/js/add_line_break.js
+++ admin/js/add_line_break.js
@@ -1,12 +1,12 @@
 function tt_add_line_break(event) {
     var key = event.which || event.keyCode;
     var field_id = document.activeElement.id;
-    var newLine = "\r\n";
+    //var newLine = "\r\n";
 
     //if user pressed enter
     if (key == '13') {
         //add a line break to the field and stop the form submit from running
-        document.getElementById(field_id).value = document.getElementById(field_id).value + newLine;
+        document.getElementById(field_id).value = document.getElementById(field_id).value + "\r\n";
         //don't submit form
         return false;
     } else {
