aa0ffe8ff20042eb44da44a63f4b77b1f49526f7
GermanPearls <amy@logicallytech.com>
2023-05-05 09:49:00 -0400

    fix project not changing with client change
    on cf7 forms should be looking for name of control, not id

--- inc/js/get_projects_for_client.js
+++ inc/js/get_projects_for_client.js
@@ -1,11 +1,11 @@
 //Update Project List When Client Name is Updated
 function tt_update_project_dropdown() {
-  var clientField = jQuery(".tt-form").find("#client-name")[0];
+  var clientField = jQuery(".tt-form").find("[name='client-name']")[0];
   if (!clientField) {
     clientField = document.getElementById(jQuery(".tt-form").find("label:contains(Client)").prop("for"));
   }   
 
-  var projectField = jQuery(".tt-form").find("#project-name")[0];
+  var projectField = jQuery(".tt-form").find("[name='project-name']")[0];
   if (!projectField) {
     projectField = document.getElementById(jQuery(".tt-form").find("label:contains(Project)").prop("for"));
   }   
