16af66f8b9fadac8f8261f99008503e8926ee7de
GermanPearls <amy@logicallytech.com>
2023-06-15 09:15:53 -0400

    fix typo causing fatal error

--- inc/function-tt-get-IDs-from-common-names.php
+++ inc/function-tt-get-IDs-from-common-names.php
@@ -16,7 +16,7 @@ namespace Logically_Tech\Time_Tracker\Inc;
 function get_client_id_from_name($client_name) {
   global $wpdb;
   $client_id_search_string = $wpdb->prepare('SELECT ClientID FROM tt_client WHERE Company= "%s"', $client_name);
-  $client_id_search_result = task_list_search_string($client_id_search_string);
+  $client_id_search_result = tt_query_db($client_id_search_string);
   if ($client_id_search_result) {
     $client_id = $client_id_search_result[0]->ClientID;
     return $client_id;    
@@ -36,7 +36,7 @@ function get_project_id_from_name($project_name) {
   } else {
     global $wpdb;
     $project_id_search_string = $wpdb->prepare('SELECT ProjectID FROM tt_project WHERE PName= %s', $project_name);
-    $project_id_search_result = task_list_search_string($project_id_search_string);
+    $project_id_search_result = tt_query_db($project_id_search_string);
     if (!empty($project_id_search_result)) {    
       $project_id = $project_id_search_result[0]->ProjectID;
     } else {
@@ -54,7 +54,7 @@ function get_project_id_from_name($project_name) {
 function get_task_id_from_name($task_name) {
   global $wpdb;
   $task_id_search_string = $wpdb->prepare('SELECT TaskID FROM tt_task WHERE TDescription= "%s"', $task_name);
-  $task_id_search_result = task_list_search_string($task_id_search_string);
+  $task_id_search_result = tt_query_db($task_id_search_string);
   if ($task_id_search_result) {
     return $task_id_search_result[0]->TaskID;
   } else {
