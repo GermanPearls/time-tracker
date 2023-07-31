4e169ebc743804b1fb6e54929ca2e1fdba4df792
GermanPearls <amy@logicallytech.com>
2023-07-31 16:28:23 -0400

    update table when dropdown value changed in table cell

--- inc/js/update_table.js
+++ inc/js/update_table.js
@@ -1,10 +1,15 @@
 function updateDatabase(editableObj, tablename, tablekey, column, id) {
 	//**Ref: https://phppot.com/php/php-mysql-inline-editing-using-jquery-ajax/***/
+	if (editableObj.innerHTML.indexOf("<select") !== -1) {
+		var val = jQuery(editableObj).find("selected").val();
+	} else {
+		var val = editableObj.innerHTML;
+	}
 	var send = {
 		'security': wp_ajax_object_tt_update_table.security,
 		'action': 'tt_update_table',
 		'field': column,
-		'value': editableObj.innerHTML,
+		'value': val,
 		'id': id,
 		'table': tablename,
 		'id_field': tablekey
