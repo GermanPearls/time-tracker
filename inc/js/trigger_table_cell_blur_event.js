6a643fe42e41cc261718e75fbaa055211ce8db17
GermanPearls <amy@logicallytech.com>
2024-11-08 16:28:20 -0500

    fix onblur event trigger

--- inc/js/trigger_table_cell_blur_event.js
+++ inc/js/trigger_table_cell_blur_event.js
@@ -1,15 +1,17 @@
 function hasOnBlurHandler(element) {
     //Google AI
-    return !!jQuery(element).data('events') && !!jQuery(element).data('events').blur;
+    //return !!jQuery(element).data('events') && !!jQuery(element).data('events').blur;
+    return jQuery(element).on('blur');
   }
   
 function trigger_table_cell_blur_event(elem) {
     prnt = jQuery(elem).parents().get(0);
     if (hasOnBlurHandler(prnt)) {
         prnt.onblur();
-    }
-    prnt_td = jQuery(elem).parents().closest('td').get(0);
-    if (hasOnBlurHandler(prnt_td)) {
-        prnt_td.onblur();
+    } else {
+        prnt_td = jQuery(elem).parents().closest('td').get(0);
+        if (hasOnBlurHandler(prnt_td)) {
+            prnt_td.onblur();
+        }
     }
 }
